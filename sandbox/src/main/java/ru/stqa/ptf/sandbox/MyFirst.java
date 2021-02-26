f46bf7b0c5e964e27ead21715cc0535edbd17708
Alexey1918 <alex@vinnov.su>
2021-02-27 02:31:38 +0300

    Программа расчета расстояния

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirst.java
@@ -3,7 +3,12 @@ package ru.stqa.ptf.sandbox;
 public class MyFirst{
       
      public static void main(String[] args){
-       System.out.println("Hello , world");
+
+         Point a = new Point(3,4);
+         Point b = new Point(5,6);
+
+         System.out.println("Расстояние равно " + a.distance(a,b) + " см.");
+
 
    }
 }
