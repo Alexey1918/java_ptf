6036f15690e527f964a520f059b69e72aa926a1d
Alexey1918 <alex@vinnov.su>
2021-04-27 22:08:53 +0300

    9,2 не находит класс в пакете

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.soap;
+
+
+import com.lavasoft.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp(){
+        String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("192.168.5.242");
+        System.out.println(ipLocation);
+    }
+}
