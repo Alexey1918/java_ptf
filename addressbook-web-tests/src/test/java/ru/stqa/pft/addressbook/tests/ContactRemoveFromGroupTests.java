902653518cfeaef2d25e96085052896c083f8e04
Alexey1918 <alex@vinnov.su>
2021-05-17 23:01:20 +0300

    Задание №21: Организовать запуск тестов на сервере непрерывной интеграции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactRemoveFromGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactRemoveFromGroupTests.java
@@ -7,6 +7,7 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -29,6 +30,7 @@ public class ContactRemoveFromGroupTests extends TestBase{
                     .withMobilePhone("+375290000000")
                     .withEmail("dummyemail@gmail.com"));
         }
+
     }
 
 
